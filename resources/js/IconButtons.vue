24b30c78bb4c70571f300b7fe415c0beecfa5390
Matt Rademacher <mrademacher@gmail.com>
2019-09-19 21:58:42 -0700

    move icon links out to own module

---
+++ resources/js/IconButtons.vue
@@ -0,0 +1,45 @@
+<template>
+    <v-flex>
+        <v-tooltip color="black" bottom>
+            <template v-slot:activator="{ on }">
+                <v-btn v-on="on" class="mx-5 px-5" icon href="https://github.com/mattquest">
+                    <v-icon color="white" size="42px">fab fa-github</v-icon>
+                </v-btn>
+            </template>
+            <span>Github</span>
+        </v-tooltip>
+        <v-tooltip color="orange" bottom>
+            <template v-slot:activator="{ on }">
+                <v-btn
+                    v-on="on"
+                    target="_blank"
+                    class="mx-5 px-5"
+                    icon
+                    href="https://stackoverflow.com/users/2227763/wheelmaker"
+                >
+                    <v-icon color="white" size="42px">fab fa-stack-overflow</v-icon>
+                </v-btn>
+            </template>
+            <span>Stack Overflow</span>
+        </v-tooltip>
+        <v-btn class="mx-5 px-5" icon @click="toggleContact()">
+            <v-icon color="white" size="42px">fas fa-envelope</v-icon>
+        </v-btn>
+    </v-flex>
+</template>
+
+<script>
+    export default {
+        name: "IconButtons",
+
+        methods: {
+            toggleContact() {
+                this.$emit('toggleContact');
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- resources/js/WhHome.vue
+++ resources/js/WhHome.vue
@@ -10,33 +10,7 @@
                         RADEMACHER
                     </v-flex>
                     <v-row justify="center" max-width="200">
-                        <v-flex>
-                            <v-tooltip color="black" bottom>
-                                <template v-slot:activator="{ on }">
-                                    <v-btn v-on="on" class="mx-5 px-5" icon href="https://github.com/mattquest">
-                                        <v-icon color="white" size="42px">fab fa-github</v-icon>
-                                    </v-btn>
-                                </template>
-                                <span>Github</span>
-                            </v-tooltip>
-                            <v-tooltip color="orange" bottom>
-                                <template v-slot:activator="{ on }">
-                                    <v-btn
-                                        v-on="on"
-                                        target="_blank"
-                                        class="mx-5 px-5"
-                                        icon
-                                        href="https://stackoverflow.com/users/2227763/wheelmaker"
-                                    >
-                                        <v-icon color="white" size="42px">fab fa-stack-overflow</v-icon>
-                                    </v-btn>
-                                </template>
-                                <span>Stack Overflow</span>
-                            </v-tooltip>
-                            <v-btn class="mx-5 px-5" icon @click="toggleContact()">
-                                <v-icon color="white" size="42px">fas fa-envelope</v-icon>
-                            </v-btn>
-                        </v-flex>
+                        <icon-buttons @toggleContact="toggleContact"></icon-buttons>
                     </v-row>
                     <v-expand-transition>
                         <v-row class="pt-5 mt-5" justify="center" v-show="showContactForm">
@@ -48,15 +22,16 @@
                         </v-row>
                     </v-expand-transition>
                 </v-col>
-
             </v-row>
         </v-container>
     </v-app>
 </template>
 
 <script>
+    import IconButtons from "./IconButtons";
     export default {
         name: "Home",
+        components: {IconButtons},
         data: () => ({
             showContactForm: false,
         }),
