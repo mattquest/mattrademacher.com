d293b1afbfe7c03ce93bec66f45d5302923c522e
Matt Rademacher <mrademacher@gmail.com>
2019-12-12 15:22:08 -0800

    ggl analytics

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,8 +15,14 @@
                 logoImage: "{{ asset('/logo.svg') }}",
             };
         </script>
-
-
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-154495712-1"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'UA-154495712-1');
+        </script>
     </head>
     <body>
         <div id="app">
